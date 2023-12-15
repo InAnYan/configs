372f904ff452aa04bc03161a8989c3602aee86b4
InAnYan <ruslanpopov11512@gmail.com>
2023-12-15 19:45:06 +0200

    commit

--- useful_programs.sh
+++ useful_programs.sh
@@ -1,4 +1,4 @@
-sudo apt install gcc g++ clang clangd clang-tidy cppcheck make cmake automake emacs python3 sbcl gdb lldb git gh opam ocaml racket tree qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils virtualbox-qt python3-pip
+sudo apt install gcc g++ clang clangd clang-tidy cppcheck make cmake automake emacs python3 sbcl gdb lldb git gh opam ocaml racket tree qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils virtualbox-qt python3-pip gnupg software-properties-common ghex
 
 raco pkg install brag
 
