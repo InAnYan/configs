8cbd7388e634b702cf91dcbdf80eb8fe19851f9c
InAnYan <ruslanpopov1512@gmail.com>
2025-03-13 07:58:40 +0000

    Update from netbook

--- export/de-netbook.sh
+++ export/de-netbook.sh
@@ -1,5 +1,11 @@
 # sh /mnt/data/projects/configs/export/lemonbar.sh | lemonbar -p &
 
+export EDITOR="emacsclient --create-frame"
+
+emacs --daemon &
+
+eval $(ssh_agent)
+
 picom &
 
 polybar -c /mnt/data/projects/configs/export/polybar-netbook-config.ini &
--- export/emacs-init-netbook.el
+++ export/emacs-init-netbook.el
@@ -1,6 +1,9 @@
 ;; [[file:../emacs-init-netbook.org::*Personal constants][Personal constants:1]]
 (defconst SAVES_DIR
   "/mnt/data/misc/file-saves")
+
+(defconst YAS_SNIPPET_DIR
+  "/mnt/data/projects/yas-snippets")
 ;; Personal constants:1 ends here
 
 ;; [[file:../emacs-init-netbook.org::*Global modes][Global modes:1]]
@@ -41,3 +44,117 @@
       kept-old-versions 2
       version-control t)
 ;; Backups:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*Package setup][Package setup:1]]
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+;; Package setup:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*=use-package=][=use-package=:1]]
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+;; =use-package=:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*Multi-term][Multi-term:1]]
+(use-package multi-term
+  :custom
+  (multi-term-program "/bin/zsh"))
+;; Multi-term:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*Copy/pasting][Copy/pasting:1]]
+(use-package xclip
+  :config
+  (xclip-mode 1))
+;; Copy/pasting:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*Flycheck][Flycheck:1]]
+(use-package flycheck)
+;; Flycheck:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*LSP][LSP:1]]
+(use-package lsp-mode
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-inlay-hint-enable t)
+  ;; These are optional configurations. See https://emacs-lsp.github.io/lsp-mode/page/lsp-rust-analyzer/#lsp-rust-analyzer-display-chaining-hints for a full list
+  ;; (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  ;; (lsp-rust-analyzer-display-chaining-hints t)
+  ;; (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  ;; (lsp-rust-analyzer-display-closure-return-type-hints t)
+  ;; (lsp-rust-analyzer-display-parameter-hints nil)
+  ;; (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+;; LSP:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*Rust][Rust:1]]
+(use-package rustic
+  :bind
+  (:map rustic-mode-map
+        ("M-j" . lsp-ui-imenu)
+        ("M-?" . lsp-find-references)
+        ("C-c C-c l" . flycheck-list-errors)
+        ("C-c C-c a" . lsp-execute-code-action)
+        ("C-c C-c r" . lsp-rename)
+        ("C-c C-c q" . lsp-workspace-restart)
+        ("C-c C-c Q" . lsp-workspace-shutdown)
+        ("C-c C-c s" . lsp-rust-analyzer-status))
+  
+  :custom
+  (rustic-rustfmt-config-alist '((edition . "2021")))
+  
+  (lsp-eldoc-hook nil)
+  (lsp-enable-symbol-highlighting nil)
+  (lsp-signature-auto-activate nil)
+
+  (rustic-format-on-save t))
+;; Rust:1 ends here
+
+;; [[file:../emacs-init-netbook.org::*=yasnippet=][=yasnippet=:1]]
+(use-package yasnippet
+  :hook
+  (prog-mode . yas-minor-mode)
+  (text-mode . yas-minor-mode)
+
+  :custom
+  (yas-snippet-dirs (list YAS_SNIPPET_DIR))
+
+  :config
+  (yas-reload-all))
+;; =yasnippet=:1 ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eldoc-documentation-functions nil t nil "Customized with use-package rustic")
+ '(package-selected-packages '(company lsp-ui multi-term rustic xclip yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
